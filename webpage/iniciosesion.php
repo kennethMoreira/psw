1bcb3d1ae9bbcbbeb567f34820fdd4ee76e4ef5e
Ericka Toscano <etoscano1995@gmail.com>
2016-01-30 14:56:15 -0500

    Inicio sesion con Php (si vale)

--- webpage/iniciosesion.php
+++ webpage/iniciosesion.php
@@ -11,6 +11,21 @@
   <link rel="stylesheet" href="../css/bootstrap.min.css">
   <link rel="stylesheet" href="../css/main.css">
   <link rel="icon" type="image/png" href="../img/logo.png"/>
+    
+    <script type="text/javascript">
+<!--//
+function crearlink(Sel){
+if (Sel.ad.selectedIndex != 0){
+var link = Sel.ad.options[Sel.ad.selectedIndex].value
+document.write("<a href='" + link + "'>" + link + "<\/a><br>");
+document.write("<a href='javascript:history.back\(\)\;'>volver<\/a><br>");
+}
+}
+//-->
+</script>
+    
+    
+    
 </head>
 <body class="fondo">
   <header>
@@ -25,34 +40,28 @@
   
   <div class = "sesion">
     <div class="iniciar">
-        <form action="informacionpaciente.php" method="post">
+        
             <fieldset>
                 <label>Correo electronico:</label>
                 <input type="text" name="correo" /> <br>
                 <label>Especialidad</label>
-                <input type="text" name="especilidad" /> <br>
                 <label>Contraseña:</label>
                 <input type="text" name="contra" /> <br>
-                <input type="submit" value="enviar" />
-                <?php
 
-                    if (isset($_SESSION['nombre'])){
-	                   session_destroy();
-	                   echo "Se ha destruido sesion exitosamente <br/>";
-	                   echo "<a href='index.php'>Volver</a>";
-
-                    }else{
-
-	                   echo "ERROR.. <br/>";
-	                   echo "<a href='index.php'>Volver</a>";
-
-                    }
-                    ?>
                 
-            </fieldset>
-        </form>
-<div><a href="casosClinicos.html" class="btn btn-primary">Iniciar sesión paciente</a></div>
-<div><a href="pacientesASerTratados.html" class="btn btn-primary">Iniciar sesión estudiante</a></div>
+<form action='../mvc/Colector de Objetos/ingresarUsuario.php'  method="post">
+                <SELECT NAME='selCombo' SIZE=1 > 
+                    <OPTION VALUE='1'>Estudiante</OPTION>
+                    <OPTION VALUE='2'>Paciente</OPTION>
+                    <input type='submit' value='enviar' /><br>
+
+                </SELECT>
+</form>
+                
+                    </fieldset>
+                
+                
+            
     </div>
   </div>
 
