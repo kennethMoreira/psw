04d9c508ecbce3a7fb6ae519b95e4b84ff3c3ae6
Ericka Toscano <etoscano1995@gmail.com>
2016-02-04 21:08:37 -0500

    Index rediseñado

--- index.php
+++ index.php
@@ -25,56 +25,160 @@
 	<script src="js/ie10-viewport-bug-workaround.js"></script>
 
 </head>
+    
 <body>
-	<div class="container-fluid">
-		<div class="main row">
-			<div class="cabecera">
-				
-					<a class="col-xs-4 col-sm-4 col-md-1 col-lg-1" href="index.php"><img src="img/logo.png" alt="Inicio"></a>
-					<div class="sist col-xs-1 col-sm-1 col-md-1 col-lg-1">Sistema </div>
-					<div class="dent col-xs-1 col-sm-1 col-md-1 col-lg-1">Dental</div>
-				
-			</div>
+    
+     <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <!-- El logotipo y el icono que despliega el menú se agrupan
+         para mostrarlos mejor en los dispositivos móviles -->
+    <div class="menu navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse"
+              data-target=".navbar-ex1-collapse">
+        <span class="sr-only">Desplegar navegación</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <!--logo-->  
+    	<div class="logoI">
+        	<a class="navbar-brand" href="index.html"><img src=" " class="img-responsive"></a>
 		</div>
-	</div>
+    
+    
+ </div>
+  
 
-	<div class="container-fluid">
-		<div class="main row">
+    <!-- Agrupar los enlaces de navegación, los formularios y cualquier
+         otro elemento que se pueda ocultar al minimizar la barra -->
+    <div class="menus collapse navbar-collapse navbar-ex1-collapse">
+        
+
+  
+      <ul class="nav navbar-nav navbar-right">
+          <li><a href="webpage/iniciosesion.php" title="Inicie sesion"><img class="icono" src="img/log-in.png"></a></li>
+          <li><a href="webpage/registro-estudiante.html" title="Registrar"><img class="icono" src="img/refresh.png"></a></li>
+          <li><a href="webpage/ayuda.php" title="Ayuda"><img class="icono" src="img/help.png"></a></li>
+
+          <li><a href="#"> </a></li>
+        
+      </ul>
 
 
-			<div class="col-xs-12 col-sm-8 col-md-9 col-lg-9">
-					<video class="vid" id="video_background" preload="auto" autoplay="autoplay" muted="muted" loop="loop"> 
-               			<source src="video/odon.mp4" type="video/mp4" />
+    </div>
+  </nav>
+    
+    <div class="container-fluid">
+        
+        
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<video class="vid" id="video_background" preload="auto" autoplay="autoplay" muted="muted" loop="loop"> 
+           			<source src="video/odon.mp4" type="video/mp4" />
             
-        			</video> 
+        	</video> 
+        </div>
+        <div class="cajaFantasma">
+            
+              <!-- Hero Section -->
+            
+                <h2 class="logo">Sistema <span class="light">Dental</span></h2>
+                <p class="tagline">Amamos tu sonrisa</p>
+                <a class="ghost" href="#1">¡Comencemos!</a>
 				
-			</div>
-		
-			<aside class="cuerpo col-xs-12 col-sm-4 col-md-3 col-lg-3">
-				
-					<h3>Bienvenidos</h3>
-					
+			
+		</div>
+        </div>
+        
+        <div class="container-fluid">
+        <div class="cuerpo">
+            <section id="1">
+            <div class="texto" >
+                
+
+            <h3>Bienvenidos</h3>
 					Nuestro propósito es establecer una comunicación entre estudiantes de odontología y personas de escasos recursos económicos que padecen de enfermedades dentales.
 					<br>
 					<br>
 					Logramos que los estudiantes cumplan con los casos clínicos requeridos para aprobar sus materias prácticas, dando un tratamiento gratuito a las patologías bucales de las personas interesadas; siempre bajo la dirección de profesionales, asignados a la cátedra de las diferentes materias donde se dará tratamiento a los pacientes.
-					
-					<h3>¡Comenzemos!</h3>
-					
-					
-						<h3><a href="webpage/iniciosesion.php" class="btn btn-lg btn-success">Iniciar sesion</a><br></h3>	
-						<h3><a href="webpage/registro-estudiante.html" class="btn btn-lg btn-success">  Registro estudiante  </a><br></h3>
-						<h3><a href="webpage/registro-paciente.html" class="btn btn-lg btn-success">  Registro paciente  </a><br></h3>
-					
-
-			</aside>
-	</div>
-
-	<div class="main row">
-		<div class="pie">		
-				
-				<p>© 2015 Sistema Dental. Todos los derechos reservados|PSW</p>
+                    
+                <div class="main row">
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            	<div class="imagen" align="center">
+				<img src="img/hacer.png" class="img-responsive">
+				¿Que hacemos?
+                    <span>Brindar una facilidad de comunicacion entre paciente/doctor</span>
+				</div>
 			</div>
+            
+            
+			<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+				<div class="imagen" align="center">
+				<img src="img/objetivo.png" class="img-responsive">
+				Objetivos
+                    <span>Liderar en el mercado de salud en Internet</span>
+				</div>
+			</div>
+            
+          
+
+			<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+				<div class="imagen" align="center">
+				<img src="img/mision.png" class="img-responsive">
+				Nuestra misión
+                    <span>Ser la mejor aplicacion web, con servicios profesionales, integrales y personalizados para nuestros pacientes.</span>
+				</div>
+			</div>
+
+         
+			<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+				<div class="imagen" align="center">
+				<img src="img/vision.png" class="img-responsive">
+				Nuestra Visión
+                    <span>Liderar en el cuidado de la salud bucal</span>
+				</div>
+			</div>
+            
+           </div>
+                
+                </div>		
+                </section>
+        </div>
+        
+    
+    <!-- 
+	<div class="container-fluid">
+		<div class="main row">
+			<div class="cabecera">
+				
+					<a class="col-xs-4 col-sm-4 col-md-1 col-lg-" href="index.php"><img src="img/logo.png" alt="Inicio"></a>
+                    <div class="logo">Sistema Dental
+					
+                      <p>Nuestra pasión es su sonrisa</p></div>
+
+				
+			</div>
+		</div>
+	</div>
+!-->
+    
+    
+		
+
+	<div class="pie main row">
+		<div class="pieD">		
+            
+            <a href="facebook.com"><img class="iconos"src="img/social-facebook-outline.png"></a>
+            <a href="twitter.com"><img class="iconos" src="img/social-twitter-outline.png"></a>
+            <a href="instagram.com"><img class="iconos" src="img/social-instagram-outline.png"></a>
+            
+	
+        </div>
+        
+        <div class="pieI">
+            <a href="webpage/nosotros.php">Nosotros</a>
+            <br>
+            <a href="webpage/servicios.php">Servicios</a>
+        </div>
+        <div class="medio">		<p>© 2015 Sistema Dental. Todos los derechos reservados|PSW</p></div>
 	</div>
 
 	
