427cdf57bd5ce6bc5a76939a738a411cf606888d
Ericka Toscano <etoscano1995@gmail.com>
2016-02-05 20:01:51 -0500

    Logout y demas paginas

--- webpage/casosClinicos.php
+++ webpage/casosClinicos.php
@@ -2,76 +2,16 @@
 	session_start();
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
 
-<!doctype html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Casos Clinicos</title>
-     <link rel="stylesheet" href="../css/style.css" type="text/css" />
-     <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css" />
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-</head>
-   
+    <?php include '../plantillasPhp/cabecar.php' ; ?>  
         
-    
-<body>
-     
-       <div class= "visible-lg mcabecera row">
-           <div class="col-lg-1"><a href="../index.php"><img id="logo" src="../img/logo.png" alt = "icono1"></a></div>
-           <div class="visible-lg col-lg-3"> <h1> Sistema dental </h1> </div>
-           
-
-	   <div class="row">
-           <div class="visible-lg col-lg-10">
-                <nav class = "navegacion nav-justified">
-                    
-                     
-                    
-                       
-                       <a class ="menu"  href="pacientesASerTratados.html" > Mis pacientes</a>
-                       <a class ="menu"  href="informacionpaciente.html" > Informacion Pacientes</a>
-                       <a class ="menu"  href="iniciosesion.html" > Cerrar sesion</a>
-                    
-                </nav>
-            </div>
-          </div>
-
-
-       </div>  
-       
-       
-
-	 
-
-
-       <div class= "hidden-lg cabeceramobil row">
-           <div class="col-xs-2"> <img id="logo" src="../img/logo.png" alt = "icono1"> </img>  </div>
-           <div class="hidden-lg col-xs-9"> <h1> Casos Clinicos </h1> </div>
-           
-        
-       </div>  
-       <div class="hidden-lg row">
-           <div class=" col-xs-3">
-                <nav role="presentation" class="dropdown">
-                       
-                       <a href="#" class ="dropdown-toggle" data-toggle="dropdown"  > Menu <span class="caret"></span></a>
-                           <ul class="dropdown-menu">
-                               <li role="presentation" class="active"><a href="pacientesASerTratados.html" > Mis pacientes</a></li>
-                               <li role="presentation" class="active"><a href="informacionpaciente.html" > Informacion Pacientes</a></li>
-                               <li role="presentation" class="active"><a href="iniciosesion.html" > Cerrar Sesion</a></li>
-                           </ul>
-                </nav>
-            </div>
-          </div>
-       
-       
-       
-    
+ 
               
              <div class="titulo row">
                  
-                 <div class="visible-lg"> <img id="pasante" src="../img/pasante.png" alt=""> <span> Casos Clinicos</span> </img> </div>
+                 <div class="visible-lg"> <img id="pasante" src="../img/pasante.png" alt=""> <span> Casos Clinicos</span> 
                  
                  
              </div>
@@ -109,14 +49,10 @@
                  
              </div>
              
-             <footer class="visible-lg">
-                  
-                  <div > <p>© 2015 Sistema Dental. Todos los derechos reservados</p></div>
-             </footer>
-            
+           
+             <?php include '../plantillasPhp/foot.php' ; ?>  
             <script src="../js/jquery.min.js"></script>
             <script src="../js/bootstrap.min.js"></script>
             
-            
-</body>
+   
 </html>
