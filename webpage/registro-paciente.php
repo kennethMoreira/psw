0534102ef6a552f01be8e52c9a6a70fbb3832baf
Rafael Barragán <rafabarr@espol.edu.ec>
2016-02-05 21:35:51 -0500

    Desarrollo código PHP en base a archivo 'registro-estudiante.php'

--- webpage/registro-paciente.php
+++ webpage/registro-paciente.php
@@ -1,3 +1,8 @@
+<?php
+$action=$_REQUEST['action'];
+if($action=="") // Mostrar el contenido de la página web
+{
+?>
 <!DOCTYPE html> 
 <html lang="es-EC">
 	<head>
@@ -112,3 +117,70 @@
 		
 	</body> 
 </html>
+<?php
+}
+else  // Obtener datos, verificarlos y enviar correo con mensaje.
+{
+	if(isset($_POST['email'])) {
+	$emailTo="rafabarr@espol.edu.ec";
+	$emailSubject="[Prueba] Notificación de nuevo estudiante registrado.";
+
+		function died($error) {
+		$errorMessage="Lo sentimos pero hay error(es) en el formulario.\nPor favor intente nuevamente.\n\n".$errorMessage;
+		echo"<script>alert(\"$errorMessage\")</script>";
+		echo"<script type='text/javascript'>window.history.back();</script>";
+		die();
+		}
+
+		if(!isset($_POST['nombres']) || !isset('registration-number') ||
+		   !isset($_POST['nivel-estudio']) || !isset('phone') ||
+		   !isset($_POST['email']) || !isset($_POST['pwd']))
+		{died('Lo sentimos, faltan datos por ingresar en el formulario.')}
+
+	$nombres=$_POST['nombres'];
+	$registrationNumber=$_POST['registration-number'];
+	$nivelEstudio=$_POST['nivel-estudio'];
+	$phone=$_POST=['phone'];
+	$email=$_POST=['email'];
+	$pwd=$_POST=['pwd'];
+	$errorMessage="";
+
+	$emailRegex='/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+	if(!(preg_match)($emailRegex,$email))
+	{$errorMessage.='La direcci\xF3n de email ingresada no es v\xE1lida.\n';}
+
+	if(strlen($message)<2)
+	{$errorMessage.='El mensaje ingresado no es v\xE1lido.\n';}
+
+	if(strlen($errorMessage)>0)
+	{died($errorMessage);}
+
+	$emailMessage="Detalles del formulario:\n\n";
+
+	function cleanString($string) {
+	$bad=array("content-type","bcc:","to:","cc:","href");
+	return str_replace($bad,"",$string)
+	}
+
+	$emailMessage.=cleanString($nombres)."\n";
+	$emailMessage.=cleanString($registrationNumber)."\n";
+	$emailMessage.=cleanString($nivelEstudio)."\n";
+	$emailMessage.=cleanString($phone)."\n";
+	$emailMessage.=cleanString($email)."\n";
+	$emailMessage.=cleanString($pwd)."\n";
+
+	$headers='Nuevo mensaje enviado a través del formulario de registro.'."\r\n".'PHP/'.phpversion();
+
+	@mail($emailTo, $emailSubject, utf8_decode($emailMessage), utf8_decode($headers), '-f'.$email);
+
+?>
+
+	<script type='text/javascript'>alert('Registro completado. \nMuy pronto nos pondremos en contacto con usted.')</script>
+	<script type='text/javascript'>location.assign("https://serene-bastion-5108.herokuapp.com/");</script>
+
+<?php
+
+}
+
+?>
