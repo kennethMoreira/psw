1083943775ee7d6dcaa900f13ed11c430da6d8e6
Ericka Toscano <etoscano1995@gmail.com>
2016-02-05 19:50:17 -0500

    Logout

--- plantillasPhp/cabecar.php
+++ plantillasPhp/cabecar.php
@@ -1,15 +1,33 @@
-	<div class="container-fluid">
+<head>
+  <meta charset="UTF-8">
+  <title>Iniciar sesión</title>
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/generico.css">
+  <link rel="icon" type="image/png" href="../img/logo.png"/>
+    
+        </head>
+<body>
+  	
+<div class="container-fluid">
 		<div class="main row">
 			<div class="cabecera">
 				
 					<a class="col-xs-4 col-sm-4 col-md-1 col-lg-" href="../index.php"><img src="../img/logo.png" alt="Inicio"></a>
-                    <div class="logotipo">Sistema Dental
-                    <p>Nuestra pasión es su sonrisa</p></div>				
+            <div class="logotipo">Sistema Dental
+             <p>Nuestra pasión es su sonrisa</p></div>				
 			</div>
-                    <div class="salir">
-            <a href="../index.php"><img src="../img/log-out.png"></a>
+        
+        <div class="salir">
+          <?php
+            echo "<a href='../mvc/ColectorDeObjetos/logout.php'><img src='../img/log-out.png'></a>"
+          ?>
+          
         </div>
+
 		</div>
         
 
 	</div>
+    
