c8c2cb86ef2d64ee1c387d8d49d7efd8204a1a75
kennethMoreira <kenneth_moreira@gmail.com>
2016-02-17 12:59:01 -0500

    Crear sexo

---
+++ admin/sexo/sexo_Create.php
@@ -0,0 +1,29 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+	<form id="frm_trans" name="frm_trans" action ="sexo_insert.php" method ="post" class="form-horizontal">
+    <div class="form-group">
+        <label class="control-label col-xs-3" >id:</label>
+        <input MaxLength="3" name="id"  class="form-control" placeholder="id">
+    </div>
+    <div class="form-group">
+        <label class="control-label col-xs-3">Descripcion:</label>
+            <input type="text" name="descripcion" class="form-control" placeholder="Descripcion" required>
+    </div>
+	
+    
+    <br>
+    <div class="form-group">
+		<input type="submit" name="registro" class="btn btn-default" value="Registrar">
+		<input type="reset" class="btn btn-primary" value="Limpiar">
+		<br>
+		<a href="sexoView.php">Volver</a>
+    </div>
+  </form>
+
+</html>
