c3176cdd8d6db4092bebf780e436a060eacb662e
kennethMoreira <kenneth_moreira@gmail.com>
2016-02-17 13:04:03 -0500

    Vista Sexo

---
+++ admin/sexo/sexoView.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>HTML5</title>
+<meta charset="utf-8">
+
+</head>
+
+<body>
+
+<header>
+  <h1>Administración Sexo</h1>
+</header>
+
+    <?php include 'sexo_list.php' ; ?>  
+
+    
+
+ <br>
+ <a href="sexo_Create.php" >Nuevo Sexo</a> 
+ <br>
+  <a href="../administracion.php" >Volver a administracion</a> 
+
+
+<footer>
+</footer>
+
+</body>
+</html>
