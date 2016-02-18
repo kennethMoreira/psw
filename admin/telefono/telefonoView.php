19d91bdf9870fb1be1ff39e16ae259075a7dd5e7
kennethMoreira <kenneth_moreira@gmail.com>
2016-02-17 19:57:44 -0500

    vista de telefonos

---
+++ admin/telefono/telefonoView.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Telefono</title>
+<meta charset="utf-8">
+
+</head>
+
+<body>
+
+<header>
+  <h1>Administración telefono</h1>
+</header>
+
+    <?php include 'telefono_list.php' ;
+   echo "<br>" ;
+    echo "  <a href='telefono_Create.php' >Nuevo telefono</a> " ;
+    echo "<br> ";
+    echo "<br> ";
+        echo "  <a href='../administracion.php' >Volver</a> " ;
+
+    ?>  
+
+    
+
+ 
+
+<footer>
+</footer>
+
+</body>
+</html>
